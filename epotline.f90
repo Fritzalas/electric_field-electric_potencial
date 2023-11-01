846ccda17913f1a6995d70f6010a1c0fd5fe4a81
Fritzalas Kosmas <144057272+Fritzalas@users.noreply.github.com>
2023-11-01 10:11:26 +0200

    Add files via upload

--- epotline.f90
+++ epotline.f90
@@ -8,8 +8,13 @@ program epotline
   integer               :: ndraw,i,j         !The ndraw is for the number of the lines to draw and i,j is for do loops.
   real                  :: theta             !H gonia ypologismou tis kyklikis epifaneias gyro apo to fortio
   real,parameter        :: PI=3.141592654    !Exoume san parameter (static variable) tin stathera pi gia tous trigonometrikous ypologismous
-  real                  :: L                 !Exoume mia metavliti typou real pou mas orizei to tetragono sxediasmou ton isodynamikon epifaneion
+  real                  :: L1,L2             !Exoume dyo metavlites typou real pou mas orizei to parallilogrammo sxediasmou ton isodynamikon epifaneion
   real                  :: rmax,rmin         !Gia na kseroume kathe fora poso konta kai makria eimaste sto pio kontino kai makrino fortio antistoixa
+  real                  :: Vol               !Gia ton ypologismo tou dynamikou se kathe simeio.
+  real,parameter        :: dl=0.0001         !The step that we are going to have
+  real                  :: max_V,min_V       !Maximum and minimum potencial at the radius direction
+  real                  :: dummy             !Just an useful dummmy index
+  integer               :: direction         !The direction to move to the radius direction
   !--------------------------------------------------------------------------------------------
   !User Interface:
   print *, '#Enter the number of charges:'
@@ -29,22 +34,58 @@ program epotline
   !----------------------------------------
   !Calculations:
   !We draw the epotlines in the square -L<=x<=+L and -L<=y<=+L
-  ndraw=4 !We give an initial value
-  L=1.0   !Also an initial value
-  do i=-ndraw,ndraw,1
-     do j=-ndraw,ndraw,1
-        x0=i*(L/ndraw)
-        y0=j*(L/ndraw)
-        call mdist(x0,y0,X,Y,N,rmin,rmax)
-        !Apofeygoume na pame konta se kapoio fortio
-        if (rmin >= L/(10*ndraw)) then
-           call potencial(x0,y0,X,Y,Q,N)
-        end if
-        !---------------------------------------------------------------------
+  L1=3.0  !Also an initial value
+  L2=3.0
+  ndraw=50 !We want to draw 21 potencial lines
+  !Pass from every charge
+  do direction=0,3*N
+     theta=(direction*PI*2.0)/(real(3*N))
+     do i=1,N,1
+        x0=X(i)+0.1*cos(theta)
+        y0=Y(i)+0.1*sin(theta)  !We move to the PI/4 angle
+        max_V=-HUGE(max_V)
+        min_V=HUGE(min_V)
+        do while (abs(x0)<=L1 .and. abs(y0)<=L2)
+           !We avoid go close to a charge.
+           call mdist(x0,y0,X,Y,N,rmin,rmax)
+          ! if (rmin>=real(L1)/real(40)) then
+              call vfield(x0,y0,X,Y,Q,N,Vol)
+              dummy=Vol
+              if (min_V>dummy) then
+                 min_V=Vol
+              end if
+              if (max_V<dummy) then
+                 max_V=Vol
+              end if
+           !end if
+           x0=x0+((dl)*cos(theta))
+           y0=y0+((dl)*sin(theta))
+           !--------------------------------------------------------------------
+        end do
+        x0=X(i)+0.1*cos(theta)
+        y0=Y(i)+0.1*sin(theta)  !We move to the PI/4 angle
+        do j=0,ndraw,1
+           dummy=min_V +(real(j)*((max_V-min_V)/real(ndraw)))
+           x0=X(i)+0.01*cos(theta)
+           y0=Y(i)+0.01*sin(theta)
+           do while (abs(x0)<=L1 .and. abs(y0)<=L2)
+             ! if (rmin>=real(L1)/real(400)) then
+                 call vfield(x0,y0,X,Y,Q,N,Vol)
+                 if (abs(Vol-dummy)<8e-3) then
+                    call potencial(x0,y0,X,Y,Q,N)
+                    exit
+                 end if
+                 x0=x0+(dl)*cos(theta)
+                 y0=y0+(dl)*sin(theta)
+              !end if
+           end do
+           x0=X(i)+0.1*cos(theta)
+           y0=Y(i)+0.1*sin(theta)
+        end do
+        !-------------------------------------------------------------------------
      end do
-     !-------------------------------------------------------------------------
+     !----------------------------------------------------------------------------
   end do
-  !----------------------------------------------------------------------------
 end program epotline
 !===============================================================================
 subroutine mdist(x0,y0,X,Y,N,rmin,rmax)
@@ -159,4 +200,26 @@ subroutine potencial(xin,yin,X,Y,Q,N)
   end do
   !------------------------------------------------------------------------------
 end subroutine potencial
+!================================================================================
+subroutine vfield(x0,y0,X,Y,Q,N,Vol)
+  implicit none
+  !------------------------------------------------------------------------------
+  !Declaration of variables:
+  integer             :: N
+  real,dimension(N)   :: X,Y,Q
+  real                :: x0,y0
+  real                :: Vol
+  integer             :: i,j
+  real                :: r2,xi,yi !r2 is the distance ((x0-x(i))^2+(y0-Y(i))^2)^(-1/2) and xi and yi are the inside difference between points
+  !------------------------------------------------------------------------------
+  !Calculations:
+  Vol=0.0        !We give to the potencial an initial value before the calculations
+  do i=1,N,1
+     xi=x0-X(i)
+     yi=y0-Y(i)
+     r2=(xi*xi+yi*yi)**(-0.5) !The multiplication is a better operation from division in fortran
+     Vol=Vol+Q(i)*r2
+  end do
+  !------------------------------------------------------------------------------
+end subroutine vfield
 !================================================================================
